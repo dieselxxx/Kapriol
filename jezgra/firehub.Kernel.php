9f974916185d016176cd961cef4d6ec11adcc9c1
dieselxxx <danijel.galic@outlook.com>
2021-12-13 15:57:37 +0100

    Typo u copyright-u (#11)

--- jezgra/firehub.Kernel.php
+++ jezgra/firehub.Kernel.php
@@ -5,7 +5,7 @@
  * @since 0.2.3.pre-alpha.M2
  *
  * @author Danijel Galić
- * @copyright 2021 Grafotisak d.o.o.
+ * @copyright 2022 FireHub Web Application Framework
  * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
  *
  * @version 1.0
