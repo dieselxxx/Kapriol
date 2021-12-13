1fdb489bb59901a1092c11cb9ae0166ec3e0688d
dieselxxx <danijel.galic@outlook.com>
2021-12-13 14:20:31 +0100

    Dodana instanca Sustava (#10)

--- web/index.php
+++ web/index.php
@@ -1,1 +1,16 @@
 <?php declare(strict_types = 1);
+
+/**
+ * Osnovna datoteka za pokretanje sustava
+ * @since 0.2.2.pre-alpha.M2
+ *
+ * @package Sustav\Jezgra
+ */
+use FireHub\Jezgra\Sustav;
+
+/**
+ * Pokreni sustav
+ */
+require __DIR__.'/../jezgra/firehub.Sustav.php';
+$sustav = new Sustav();
+$sustav->pokreni();
