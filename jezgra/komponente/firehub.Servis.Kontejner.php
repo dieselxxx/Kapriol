548aadafac53e8b4fb9f7e13d9bda62b2835037f
dieselxxx <danijel.galic@outlook.com>
2021-12-17 15:38:13 +0100

    Refactor koda (#16)

--- jezgra/komponente/firehub.Servis.Kontejner.php
+++ jezgra/komponente/firehub.Servis.Kontejner.php
@@ -35,6 +35,12 @@ final class Servis_Kontejner extends Kontejner {
     private const POSLUZITELJI = FIREHUB_ROOT . 'konfiguracija' . RAZDJELNIK_MAPE . 'posluzitelji.php';
 
     /**
+     * ### Lista posluzitelja
+     * @var array<string, array<string[]>>
+     */
+    private array $posluzitelji = [];
+
+    /**
      * ### Konstruktor
      * @since 0.3.0.pre-alpha.M3
      *
@@ -42,11 +48,21 @@ final class Servis_Kontejner extends Kontejner {
      * Poslužitelj servisa za trenutni servis servise.
      * </p>
      *
-     * @throws Kontejner_Greska Ukoliko postavljeni servisi nije ispravan, ne postoji poslužitelj u konfiguraciji poslužitelja ili ne postoji servis za poslužitelja u konfiguraciji poslužitelja.
+     * @throws Kontejner_Greska Ukoliko se ne može učitati datoteka sa poslužiteljima, postavljeni servisi nije ispravan, ne postoji poslužitelj u konfiguraciji poslužitelja ili ne postoji servis za poslužitelja u konfiguraciji poslužitelja.
      */
     public function __construct (
         private Servis_Posluzitelj $posluzitelj
     ) {
+
+        // učitaj datoteku sa poslužiteljima
+        $this->posluzitelji = include self::POSLUZITELJI ?? throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
+
+        // provjeri da li postoji poslužitelj u konfiguraciji poslužitelja
+        if (!array_key_exists($this->posluzitelj::class, $this->posluzitelji)) {
+
+            throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
+
+        }
 
         parent::__construct($this->servis());
 
@@ -92,20 +108,14 @@ final class Servis_Kontejner extends Kontejner {
      */
     private function servis ():string {
 
+        // provjeri da li je ispravan postavljeni servis ukoliko je naveden u poslužitelju
         if (
             !is_null($this->posluzitelj->postavljeniServis()) && // postoji unaprijed postavljeni servis
             (
-                array_key_exists($this->posluzitelj->postavljeniServis(), $this->posluzitelji()[$this->posluzitelj::class]['servisi']) !== true // postavljeni servis postoji kao servis kod trenutnog poslužitelja
+                array_key_exists($this->posluzitelj->postavljeniServis(), $this->posluzitelji[$this->posluzitelj::class]['servisi']) !== true // postavljeni servis postoji kao servis kod trenutnog poslužitelja
                 || !is_a($this->posluzitelj->postavljeniServis(), Servis_Interface::class, true) // postavljeni servis ima interface za servise
             )
         ) {
-
-            throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
-
-        }
-
-        // provjeri da li postoji poslužitelj i zadani servis u konfiguraciji poslužitelja
-        if (!array_key_exists($this->posluzitelj::class, $this->posluzitelji())) {
 
             throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
 
@@ -127,14 +137,14 @@ final class Servis_Kontejner extends Kontejner {
     private function zadaniServis ():string {
 
         // ako poslužitelj u datoteci sa poslužiteljima nema niti jednog servisa
-        if (empty($this->posluzitelji()[$this->posluzitelj::class])) {
+        if (empty($this->posluzitelji[$this->posluzitelj::class])) {
 
             throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
 
         }
 
         // prvi zapis (servis) poslužitelja
-        $prvi_element = reset($this->posluzitelji()[$this->posluzitelj::class]);
+        $prvi_element = reset($this->posluzitelji[$this->posluzitelj::class]);
 
         // ako prvi zapis poslužitelja nije niz vrijednosti ili ključ prvog zapis poslužitelja nije string
         if (!is_array($prvi_element) || !is_string(key($prvi_element))) {
@@ -143,25 +153,14 @@ final class Servis_Kontejner extends Kontejner {
 
         }
 
+        if (!is_a(key($prvi_element), Servis_Interface::class, true)) { // postavljeni servis ima interface za servise)
+
+            throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
+
+        }
+
         // vrati prvi servis iz datoteke poslužitelja
         return key($prvi_element);
-
-    }
-
-    /**
-     * ### Konfiguracijska datoteka posluzitelja
-     *
-     * Ulazna točka za konfiguraciju svih posluzitelja koja sadrži
-     * niz FQN naziva Servis Poslužitelja.
-     * @since 0.3.0.pre-alpha.M3
-     *
-     * @throws Kontejner_Greska Ukoliko se ne može učitati datoteka sa posluziteljima.
-     *
-     * @return array<string, array<string[]>> Konfiguracijska datoteka posluzitelja.
-     */
-    private function posluzitelji ():array {
-
-        return include self::POSLUZITELJI ?? throw new Kontejner_Greska(_('Ne mogu pokrenuti sustav, obratite se administratoru'));
 
     }
 
