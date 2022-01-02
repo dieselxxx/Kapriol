b4c10fe513ea5e9eed14f25bf27d9f213f251af0
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-02 14:21:55 +0100

    Dodani testni podatci (#24)

---
+++ aplikacija/FireHubTest/kontroler/firehub.Naslovna.Kontroler.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types = 1);
+
+namespace FireHub\Aplikacija\FireHubTest\Kontroler;
+
+final class Naslovna_Kontroler {
+
+    public function index () {
+
+    }
+
+    public function index2 () {
+
+    }
+
+}
---
+++ aplikacija/FireHubTest/podatci/rute/http.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Rute za HTTP pozive aplikacije
+ * @since 0.4.1.pre-alpha.M4
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Jezgra
+ */
+
+use FireHub\Jezgra\Komponente\Rute\Rute;
+
+Rute::get('/naslovna/index', [\FireHub\Aplikacija\FireHubTest\Kontroler\Naslovna_Kontroler::class, 'index']);
+Rute::get('/naslovna/index', [\FireHub\Aplikacija\FireHubTest\Kontroler\Naslovna_Kontroler::class, 'index2']);
