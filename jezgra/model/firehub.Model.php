f07d8cdf1dcf22ad941c8df4c92821e7340b79e1
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-03 17:35:02 +0100

    Napravljen mnodel (#25)

---
+++ jezgra/model/firehub.Model.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za glavni model kojeg svi ostali modeli trebaju nastaviti
+ * @since 0.4.2.pre-alpha.M4
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Model
+ */
+
+namespace FireHub\Jezgra\Model;
+
+/**
+ * ### Abstrakt klasa za glavni model
+ * @since 0.4.2.pre-alpha.M4
+ *
+ * @package Sustav\Model
+ */
+abstract class Model {
+
+}
