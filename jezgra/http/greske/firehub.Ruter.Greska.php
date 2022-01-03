6b31f4dc6bd1be063ff8755c444d430056390c0e
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-03 11:51:32 +0100

    Napravljene greške za ruter

---
+++ jezgra/http/greske/firehub.Ruter.Greska.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za Ruter greške
+ * @since 0.4.2.pre-alpha.M4
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\HTTP
+ */
+
+namespace FireHub\Jezgra\HTTP\Greske;
+
+use FireHub\Jezgra\Greske\Greska;
+
+/**
+ * ### Ruter greške
+ * @since 0.4.2.pre-alpha.M4
+ *
+ * @package Sustav\HTTP
+ */
+class Ruter_Greska extends Greska {
+
+}
