5fdbe4f44f09cb5c46172e7d40dd1ba8a9739e34
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-04 15:28:22 +0100

    Novi testovi

--- aplikacija/FireHubTest/kontroler/firehub.Naslovna.Kontroler.php
+++ aplikacija/FireHubTest/kontroler/firehub.Naslovna.Kontroler.php
@@ -3,9 +3,13 @@
 namespace FireHub\Aplikacija\FireHubTest\Kontroler;
 
 use FireHub\Jezgra\Kontroler\Kontroler;
+use FireHub\Jezgra\Atributi\Posrednici;
+use FireHub\Jezgra\Posrednici\Test3_Posrednik;
 
+#[Posrednici([Test3_Posrednik::class])]
 final class Naslovna_Kontroler extends Kontroler {
 
+    #[Posrednici([Test3_Posrednik::class])]
     public function index (\FireHub\Jezgra\Komponente\Dot\Dot $dot = null, $par1 = 'test', int $par2 = 5):string {
 
         return 'ja_sam_naslovna_index';
