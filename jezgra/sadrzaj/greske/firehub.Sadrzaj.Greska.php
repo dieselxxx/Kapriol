4dc6d20054c55bbd36907029d7cec7701ddb4708
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-04 18:14:42 +0100

    Napravljene greške za Sadrzaj (#27)

---
+++ jezgra/sadrzaj/greske/firehub.Sadrzaj.Greska.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za greške sadržaja
+ * @since 0.4.4.alpha.M4
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Jezgra
+ */
+
+namespace FireHub\Jezgra\Sadrzaj\Greske;
+
+use FireHub\Jezgra\Greske\Greska;
+
+/**
+ * ### Greške sadržaja
+ * @since 0.4.4.pre-alpha.M4
+ *
+ * @package Sustav\Jezgra
+ */
+class Sadrzaj_Greska extends Greska {
+
+}
