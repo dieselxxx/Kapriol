59dab3fcf2121dd2d6b2ece8b17969b433cc3ca1
dieselxxx <danijel.galic@outlook.com>
2022-01-05 15:50:18 +0100

    Test konfiguracije

---
+++ aplikacija/FireHubTestDva/konfiguracija/sustav/predmemorija.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za konfiguraciju parametara za predmemoriju
+ * @since 0.5.0.pre-alpha.M5
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Aplikacija\Konfiguracija
+ */
+
+return [
+
+];
