de0729a5b548679c0f4d15c1f611e2de6a6f7ecc
dieselxxx <danijel.galic@outlook.com>
2022-01-05 15:51:48 +0100

    Napravljene greške za predmemoriju (#29)

---
+++ jezgra/komponente/predmemorija/greske/firehub.Predmemorija.Greska.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za greške predmemorije
+ * @since 0.5.0.pre-alpha.M5
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Jezgra
+ */
+
+namespace FireHub\Jezgra\Komponente\Predmemorija\Greske;
+
+use FireHub\Jezgra\Greske\Greska;
+
+/**
+ * ### Klasa za greške predmemorije
+ * @since 0.5.0.pre-alpha.M5
+ *
+ * @package Sustav\Jezgra
+ */
+class Predmemorija_Greska extends Greska {
+
+}
