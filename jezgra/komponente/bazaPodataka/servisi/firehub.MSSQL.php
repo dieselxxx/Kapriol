bf874379478e286ea7035c58f358da64e577f2c1
dieselxxx <danijel.galic@outlook.com>
2022-01-07 12:29:58 +0100

    Završen MySQL servis (#30)

--- jezgra/komponente/bazaPodataka/servisi/firehub.MSSQL.php
+++ jezgra/komponente/bazaPodataka/servisi/firehub.MSSQL.php
@@ -14,7 +14,11 @@
 
 namespace FireHub\Jezgra\Komponente\BazaPodataka\Servisi;
 
+use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka;
 use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka_Interface;
+use FireHub\Jezgra\Komponente\Log\Enumeratori\Level;
+use FireHub\Jezgra\Komponente\BazaPodataka\Greske\BazaPodataka_Greska;
+use FireHub\Jezgra\Kontejner\Greske\Kontejner_Greska;
 
 /**
  * ### Servis MSSQL baze podataka
@@ -23,6 +27,50 @@ use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka_Interface;
  * @package Sustav\Jezgra
  */
 final class MSSQL implements BazaPodataka_Interface {
+
+    /**
+     * ### Konekcija na MSSQL server
+     * @var mixed
+     */
+    private mixed $konekcija;
+
+    /**
+     * ### Konstruktor
+     * @since 0.5.1.pre-alpha.M5
+     *
+     * @param BazaPodataka $posluzitelj <p>
+     * Poslužitelj servisa.
+     * </p>
+     *
+     * @throws BazaPodataka_Greska Ukoliko se ne može spojiti na MSSQL server.
+     * @throws Kontejner_Greska Ukoliko se ne može spremiti instanca Log-a ili konfiguracije.
+     */
+    public function __construct (
+        private BazaPodataka $posluzitelj
+    ) {
+
+        if (
+            !$this->konekcija = sqlsrv_connect(
+                $this->posluzitelj->host . '\\' . $this->posluzitelj->instanca . ',' . $this->posluzitelj->port,
+                [
+                    'Database' => $this->posluzitelj->baza,
+                    'UID' => $this->posluzitelj->korisnicko_ime,
+                    'PWD' => $this->posluzitelj->lozinka,
+                    'CharacterSet' => $this->posluzitelj->karakteri
+                ]
+            )
+        ) {
+
+            zapisnik(Level::UZBUNA, _('Ne mogu se spojiti na MSSQL server!'));
+            throw new BazaPodataka_Greska(_('Ne mogu se spojiti na MSSQL server!'));
+
+        }
+
+        // dodatni postavke MSSQL-a
+        sqlsrv_configure('LogSeverity', konfiguracija('baza_podataka.greske'));
+        sqlsrv_configure('LogSubsystems', konfiguracija('baza_podataka.vrste_greski'));
+
+    }
 
     public function redak ():array
     {
