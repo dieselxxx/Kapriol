26c54c88965383329adf8c6a377dcdaacae378b6
dieselxxx <danijel.galic@outlook.com>
2022-01-07 14:14:49 +0100

    Typo u docblock-u

--- aplikacija/FireHubTest/konfiguracija/sustav/baza_podataka.php
+++ aplikacija/FireHubTest/konfiguracija/sustav/baza_podataka.php
@@ -5,7 +5,7 @@
  * @since 0.5.1.pre-alpha.M5
  *
  * @author Danijel Galić
- * @copyright 2021 Grafotisak d.o.o.
+ * @copyright 2022 FireHub Web Application Framework
  * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
  *
  * @version 1.0
