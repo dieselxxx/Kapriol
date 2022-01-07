3fc7fd2e4024d49242c920b4b735a953c850accb
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-07 21:28:03 +0100

    Napravljene greške za sesiju (#32)

---
+++ jezgra/komponente/sesija/greske/firehub.Sesija.Greska.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za greške sesije
+ * @since 0.5.3.pre-alpha.M5
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Jezgra
+ */
+
+namespace FireHub\Jezgra\Komponente\Sesija\Greske;
+
+use FireHub\Jezgra\Greske\Greska;
+
+/**
+ * ### Greške sesije
+ * @since 0.5.3.pre-alpha.M5
+ *
+ * @package Sustav\Jezgra
+ */
+class Sesija_Greska extends Greska {
+
+}
