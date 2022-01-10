c2921977adefe74bf019842ffb18ce97722a3eed
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-10 20:02:41 +0100

    Komponenta baze podataka prebačena iz singletona u dohvati mod

--- jezgra/komponente/bazaPodataka/firehub.BazaPodataka.php
+++ jezgra/komponente/bazaPodataka/firehub.BazaPodataka.php
@@ -218,7 +218,7 @@ final class BazaPodataka extends Servis_Posluzitelj {
      */
     public function napravi ():object {
 
-        return (new Servis_Kontejner($this))->singleton();
+        return (new Servis_Kontejner($this))->dohvati();
 
     }
 
