4b7375389649f3098c122cd13f8ad38a6b789d5d
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-01-16 16:13:36 +0100

    Napravljene greške za slike (#41)

---
+++ jezgra/komponente/slika/greske/firehub.Slika.Greska.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * Datoteka za greške slike
+ * @since 0.6.1.alpha.M6
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Sustav\Jezgra
+ */
+
+namespace FireHub\Jezgra\Komponente\Slika\Greske;
+
+use FireHub\Jezgra\Greske\Greska;
+
+/**
+ * ### Greške slike
+ * @since 0.6.1.alpha.M6
+ *
+ * @package Sustav\Jezgra
+ */
+class Slika_Greska extends Greska {
+
+}
