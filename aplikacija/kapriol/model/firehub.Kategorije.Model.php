62c4eca8bdc8a7cccf73c4428928f90fbe2b351a
dieselxxx <danijel.galic@outlook.com>
2022-01-26 11:49:24 +0100

    UI

--- aplikacija/kapriol/model/firehub.Kategorije.Model.php
+++ aplikacija/kapriol/model/firehub.Kategorije.Model.php
@@ -133,7 +133,9 @@ final class Kategorije_Model extends Master_Model {
             $kategorija_html .= <<<Kategorija
             
                 <a class="kategorija" href="/rezultat/{$kategorija['Link']}">
-                    <img src="/slika/kategorija/{$kategorija['Slika']}" alt="" loading="lazy"/>
+                    <div class="slika_okvir">
+                        <img src="/slika/kategorija/{$kategorija['Slika']}" alt="" loading="lazy"/>
+                    </div>
                     <span class="naslov">{$kategorija['Kategorija']}</span>
                 </a>
 
