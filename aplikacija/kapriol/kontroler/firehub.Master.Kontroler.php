e809a5e172adbc633f28a0b63b01e1f8703df510
dieselxxx <danijel.galic@outlook.com>
2022-03-03 13:22:52 +0100

    ?

--- aplikacija/kapriol/kontroler/firehub.Master.Kontroler.php
+++ aplikacija/kapriol/kontroler/firehub.Master.Kontroler.php
@@ -62,6 +62,20 @@ abstract class Master_Kontroler extends Kontroler {
 
         }
 
+        if (isset($_POST['favorit_izbrisi'])) {
+
+            if (isset($_POST['ID'])) {
+
+                $id =  Validacija::Broj('Veličina', $_POST['ID'], 1, 10);
+
+                var_dump($id);
+
+                $this->model(Favorit_Model::class)->izbrisi($id);
+
+            }
+
+        }
+
     }
 
     /**
--- aplikacija/kapriol/model/firehub.Favorit.Model.php
+++ aplikacija/kapriol/model/firehub.Favorit.Model.php
@@ -14,13 +14,36 @@
 
 namespace FireHub\Aplikacija\Kapriol\Model;
 
+use FireHub\Aplikacija\Kapriol\Jezgra\Domena;
+use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka;
+use FireHub\Jezgra\Kontejner\Greske\Kontejner_Greska;
+
 /**
  * ### Favorit model
+ *
  * @since 0.1.2.pre-alpha.M1
  *
  * @package Aplikacija\Model
  */
 final class Favorit_Model extends Master_Model {
+
+    /**
+     * ### Konstruktor
+     * @since 0.1.2.pre-alpha.M1
+     *
+     * @param BazaPodataka $bazaPodataka <p>
+     * Baza podataka.
+     * </p>
+     *
+     * @throws Kontejner_Greska Ukoliko se ne može spremiti instanca Sesije.
+     */
+    public function __construct (
+        private BazaPodataka $bazaPodataka
+    ) {
+
+        parent::__construct();
+
+    }
 
     /**
      * @param int $id
@@ -36,11 +59,11 @@ final class Favorit_Model extends Master_Model {
     }
 
     /**
-     * @param string $id
+     * @param int $id
      *
      * @return bool
      */
-    public function izbrisi (string $id = ''):bool {
+    public function izbrisi (int $id):bool {
 
         if (!isset($this->sesija->procitaj('favorit')[$id])) {
 
@@ -48,7 +71,7 @@ final class Favorit_Model extends Master_Model {
 
         }
 
-        $this->sesija->izbrisiNiz('favorit', $id);
+        $this->sesija->izbrisiNiz('favorit', (string)$id);
 
         return true;
 
@@ -66,6 +89,61 @@ final class Favorit_Model extends Master_Model {
         }
 
         return 0;
+
+    }
+
+    /**
+     * ### Artikli iz favorita
+     * @since 0.1.2.pre-alpha.M1
+     *
+     * @throws Kontejner_Greska Ukoliko se ne može spremiti instanca objekta.
+     *
+     * @return array Niz artikala.
+     */
+    public function artikliFavorit ():array {
+
+        if ($this->sesija->procitaj('favorit')) {
+
+            $id = array_keys($this->sesija->procitaj('favorit'));
+
+            $sifra_array = '';
+            foreach ($id as $kljuc => $vrijednost) {
+
+                if ($kljuc === array_key_first($id)) {
+
+                    $sifra_array .= "
+                        artikliview.ID = $vrijednost
+                    ";
+
+                } else {
+
+                    $sifra_array .= "
+                        OR artikliview.ID = $vrijednost
+                    ";
+
+                }
+
+            }
+
+            $artikli = $this->bazaPodataka->tabela('artikliview')
+                ->sirovi("
+                    SELECT
+                           artikliview.ID, artikliview.Naziv, artikliview.Link, artikliview.".Domena::sqlCijena()." AS Cijena, artikliview.".Domena::sqlCijenaAkcija()." AS CijenaAkcija, slikeartikal.Slika
+                    FROM artikliview
+                    LEFT JOIN slikeartikal ON slikeartikal.ClanakID = artikliview.ID
+                    WHERE artikliview.Aktivan = 1 AND artikliview.".Domena::sqlTablica()." = 1 AND slikeartikal.Zadana = 1
+                    AND ($sifra_array)
+                    ORDER BY Naziv ASC
+                ")
+                ->napravi();
+
+            $rezultat = $artikli->niz();
+
+            return $rezultat ?: [];
+
+        }
+
+        return [];
 
     }
 
