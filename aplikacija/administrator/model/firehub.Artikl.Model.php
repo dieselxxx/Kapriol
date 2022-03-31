272d68a51cbc01f8267fc6103869e9e4db7617ca
dieselxxx <danijel.galic@outlook.com>
2022-03-31 13:50:32 +0200

    squash! ?

--- aplikacija/administrator/model/firehub.Artikl.Model.php
+++ aplikacija/administrator/model/firehub.Artikl.Model.php
@@ -291,7 +291,7 @@ final class Artikl_Model extends Master_Model {
             ")
             ->napravi();
 
-        return $karakteristike->niz();
+        return $karakteristike->niz() ?: [];
 
     }
 
