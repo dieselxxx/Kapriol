bbaff195159ead1a91f0ae968f3e5eec0ea45a35
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-04-04 11:09:20 +0200

    Update firehub.Kategorija.Model.php

--- aplikacija/administrator/model/firehub.Kategorija.Model.php
+++ aplikacija/administrator/model/firehub.Kategorija.Model.php
@@ -1,1 +1,21 @@
-ccc
+<?php declare(strict_types = 1);
+
+/**
+ * Kategorija model
+ * @since 0.1.2.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2022 Kapriol Web Trgovina
+ * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
+ *
+ * @version 1.0
+ * @package Aplikacija\Model
+ */
+
+namespace FireHub\Aplikacija\Administrator\Model;
+
+use FireHub\Aplikacija\Administrator\Jezgra\PrijenosDatoteka;
+use FireHub\Aplikacija\Kapriol\Jezgra\Validacija;
+use FireHub\Jezgra\Greske\Greska;
+use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka;
+use FireHub\Jezgra\Kontejner\Greske\Kontejner_Greska;
