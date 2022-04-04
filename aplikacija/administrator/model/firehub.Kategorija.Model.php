709f4e9d2642463bea8bdb13e34158c9f4591a9d
Danijel Galić <56673130+dieselxxx@users.noreply.github.com>
2022-04-04 12:00:53 +0200

    Update firehub.Kategorija.Model.php

--- aplikacija/administrator/model/firehub.Kategorija.Model.php
+++ aplikacija/administrator/model/firehub.Kategorija.Model.php
@@ -19,3 +19,52 @@ use FireHub\Aplikacija\Kapriol\Jezgra\Validacija;
 use FireHub\Jezgra\Greske\Greska;
 use FireHub\Jezgra\Komponente\BazaPodataka\BazaPodataka;
 use FireHub\Jezgra\Kontejner\Greske\Kontejner_Greska;
+
+/**
+ * ### Kategorija
+ * @since 0.1.2.pre-alpha.M1
+ *
+ * @package Aplikacija\Model
+ */
+final class Kategorija_Model extends Master_Model {
+
+    /**
+     * ### Konstruktor
+     * @since 0.1.2.pre-alpha.M1
+     */
+    public function __construct (
+        private BazaPodataka $bazaPodataka
+    ){
+
+        parent::__construct();
+
+    }
+
+    /**
+     * ### Kateogrija
+     * @since 0.1.2.pre-alpha.M1
+     *
+     * @param int $id
+     *
+     * @throws Kontejner_Greska
+     * @return array|false|mixed[]
+     */
+    public function kategorija (int $id):array|false {
+
+        $kategorija = $this->bazaPodataka
+            ->sirovi("
+                SELECT
+                    kategorije.ID, kategorije.Kategorija, kategorije.Slika, kategorije.CalcVelicina, kategorije.Prioritet
+                FROM kategorije
+                WHERE kategorije.ID = $id
+                LIMIT 1
+            ")
+            ->napravi();
+
+        $kategorija = $kategorija->redak();
+
+        if ($kategorija['CalcVelicina']) {$kategorija['CalcVelicina'] = true;} else {$kategorija['CalcVelicina'] = false;}
+
+        return $kategorija;
+
+    }
