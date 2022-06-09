f0b8facddbcf4bf6e7975fe9d1e5dab7789d51a5
dieselxxx <danijel.galic@outlook.com>
2022-06-09 14:27:26 +0200

    ....

--- aplikacija/kapriol/kontroler/firehub.Kosarica.Kontroler.php
+++ aplikacija/kapriol/kontroler/firehub.Kosarica.Kontroler.php
@@ -8,7 +8,7 @@
  * @copyright 2022 Kapriol Web Trgovina
  * @license GNU General Public License version 3 - [https://opensource.org/licenses/GPL-3.0](https://opensource.org/licenses/GPL-3.0)
  *
- * @version $id$ vvv
+ * @version $Id
  * @package Aplikacija\Kontroler
  */
 
